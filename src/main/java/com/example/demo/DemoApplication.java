29ced6e98f90223e03db0ba8d5fb8ce56f040dd3
Prateek Jain <prateekjainit@gmail.com>
2020-06-29 17:57:39 +0530

    Added comment

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Sprint boot application
 @SpringBootApplication
 public class DemoApplication {
 
